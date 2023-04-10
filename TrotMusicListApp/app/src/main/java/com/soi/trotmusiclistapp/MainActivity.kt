6c92b6432270227136436ed8906c3a98339d0d96
JIWON1923 <zest1923@gmail.com>
2023-04-11 05:16:39 +0900

    트로트 음악 리스트 앱 (recyclerview, navigation)

---
+++ TrotMusicListApp/app/src/main/java/com/soi/trotmusiclistapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.soi.trotmusiclistapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        // 1. navigation 만들기 (navigation.xml)
+        // 2. fragment 만들고, navigation 연결 ( just setting)
+        // 2-1 fragment에서 버튼 누르면, 이동하게 만들기 (fragment 코드 작성)
+
+
+        // 3. recycler view 만들기
+    }
+}
